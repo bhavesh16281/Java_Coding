d76806c4d4827058548396c96d0031621e00447c
bhavesh16281 <bhavesh16281@gmail.com>
2025-08-22 11:16:43 +0530

    Hello World

---
+++ Basics_Part_1/HelloBhavesh/src/main/java/com/github/bhavesh16281/HelloBhavesh/App.java
@@ -0,0 +1,9 @@
+package com.github.bhavesh16281.HelloBhavesh;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello \nBhavesh!" );
+    }
+}
